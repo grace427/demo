14e3e8af785f798ba2ad8bfcb03e86e8675f2f38
ashley <ashley@repo.mosition.co.kr>
2021-11-23 23:15:01 +0900

    d

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-// import express from 'express';
 const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("lg ces");
+
+app.get("/video", (req, res) => {
+    res.sendFile(__dirname+'/views/video.html');
 });
 
 app.listen(3000, () => {
     console.log("start! express server on port 3000")
 })
+
+
+//http://52.79.237.24:3000/
