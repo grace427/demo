eeda1c08cdf19c39f25501f207f88153606057f5
ashley <ashley@repo.mosition.co.kr>
2021-11-23 23:43:03 +0900

    aa

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.get("/video", (req, res) => {
     res.sendFile(__dirname+'/views/video.html');
 });
 
+app.get("/vimeo", (req, res) => {
+    res.sendFile(__dirname+'/views/vimeo.html');
+});
+
+app.get("/youtube", (req, res) => {
+    res.sendFile(__dirname+'/views/youtube.html');
+});
+
 app.listen(3000, () => {
     console.log("start! express server on port 3000")
 })
